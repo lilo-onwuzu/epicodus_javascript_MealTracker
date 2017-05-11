36c882a2b0254c3589c516215f2c282a9628b7a1
Lilo Onwuzu <lpr422@gmail.com>
2017-05-11 14:01:10 -0700

    push to heroku again

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var port = process.env.PORT || 3000;
 
 //Create a web server
-var http = require('http');
-http.createServer(function (request, response) {
+var https = require('https');
+https.createServer(function (request, response) {
   router.home(request, response);
 }).listen(port);
 console.log('Server running');
