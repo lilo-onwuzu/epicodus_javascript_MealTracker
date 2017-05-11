9265bcab79883f220c4247a36f9923e28d0f616a
Lilo Onwuzu <lpr422@gmail.com>
2017-05-11 14:32:25 -0700

    push to heroku again

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var app = express();
 var port = process.env.PORT || 8080;
 
 // make express look in the public directory for assets (css/js/img)
-//app.use(express.static(__dirname + '/app'));
+app.use(express.static('./'));
+app.set('view engine', 'html');
 
 // set the home page route
 app.get('/', function(req, res) {
